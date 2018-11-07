ca87bda239abaa6f3408cee03c4415f1bc5180f9
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2018-11-07 14:26:01 +0200

    setting up project with smaller components

---
+++ my-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/_Aux";
+
+class BurgerBuidlder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Build Control</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuidlder;
