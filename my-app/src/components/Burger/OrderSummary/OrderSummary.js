dc7dd524b3ddd3e6b9f294a8c215dab4c60711c9
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2018-11-17 19:39:21 +0200

    adding some lifecycle methods

--- my-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ my-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,32 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = props => {
-  const igSummary = Object.keys(props.ingredients).map(igKey => (
-    <li key={igKey}>
-      <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-      {props.ingredients[igKey]}
-    </li>
-  ));
-  return (
-    <Fragment>
-      <h3>Your Order</h3>
-      <p>Your burger with following ingredients:</p>
-      <ul>{igSummary}</ul>
-      <p>
-        <strong>Total price: {props.price} euros</strong>
-      </p>
-      <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Fragment>
-  );
-};
+class OrderSummary extends Component {
+  render() {
+    const igSummary = Object.keys(this.props.ingredients).map(igKey => (
+      <li key={igKey}>
+        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+        {this.props.ingredients[igKey]}
+      </li>
+    ));
+    return (
+      <Fragment>
+        <h3>Your Order</h3>
+        <p>Your burger with following ingredients:</p>
+        <ul>{igSummary}</ul>
+        <p>
+          <strong>Total price: {this.props.price} euros</strong>
+        </p>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Fragment>
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
